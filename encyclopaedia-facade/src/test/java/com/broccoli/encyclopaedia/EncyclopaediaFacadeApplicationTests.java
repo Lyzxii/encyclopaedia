3e2b9d89bf3294e8a3e016800320291458f8cd86
lyzxii <lyzxii@163.com>
2024-01-14 15:05:26 +0800

    init project

---
+++ encyclopaedia-facade/src/test/java/com/broccoli/encyclopaedia/EncyclopaediaFacadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broccoli.encyclopaedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncyclopaediaFacadeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
